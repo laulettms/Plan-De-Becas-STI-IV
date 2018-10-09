0971aea69341914e88e68b894c94861acab6f914
laura <dar_k_blue@live.com.mx>
2018-10-09 14:03:15 -0700

    Primer progamanum

--- programacion-c/archivo1-181009.c
+++ programacion-c/archivo1-181009.c
@@ -1,11 +1,14 @@
 #include<stdio.h>
 
 int main(){
+int numero;
+ numero = 1;
 
 printf("Hola laura\n");
 printf("Hola laura\t");
-
-
+printf("\nMi numero es %d", numero);
+numero = 3;
+printf("\nMi numero es %d\n", numero);
 }
 
 
